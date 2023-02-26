f48155f13ea59db85e8d714fb1bf35dbebc1c68e
arata <0113arata@gmail.com>
2023-02-26 19:08:27 +0900

    commit

---
+++ Notify/main.py
@@ -0,0 +1,65 @@
+#インポート
+from tkinter import messagebox
+import tkinter.ttk as ttk
+import requests
+import tkinter
+import json
+import sys
+import os
+
+#必要な情報を変数に代入
+url = 'https://notify-api.line.me/api/notify'
+
+MyToken = 'Jrb3iT6GH1JNwLIHindsyQ9PgBAKTFblfkvtk4p1hmD'
+Osyaberikai = 'T01hlGkUjXSB3xmMwm4KB9h4Wmg5tf1wVaOStInbW5q'
+
+#Tikinterの基本設定
+root = tkinter.Tk()
+root.title(u"LINE送信パネル")
+root.geometry("310x210")
+root.resizable(0,0)
+
+#ファンクションプログラム
+def main(event):
+    if EditBox1.get() == "自分":
+        TOKEN = 'Jrb3iT6GH1JNwLIHindsyQ9PgBAKTFblfkvtk4p1hmD'
+    elif EditBox1.get() == "おしゃべり会":
+        TOKEN = 'T01hlGkUjXSB3xmMwm4KB9h4Wmg5tf1wVaOStInbW5q'
+
+    TOKEN_dic = {'Authorization': 'Bearer' + ' ' + TOKEN}
+    send_dic = {'message': EditBox2.get()}
+    requests.post(url, headers=TOKEN_dic, data=send_dic)
+    EditBox2.delete(0,tkinter.END)
+
+def image():
+    print('image')
+
+
+#UI
+Static1 = tkinter.Label(text=u'誰に送信しますか？')
+Static1.pack()
+
+EditBox1 = tkinter.Entry(width=50)
+EditBox1.pack()
+
+border1=ttk.Separator(root,orient="horizontal")
+border1.pack(fill="both")
+
+Static2 = tkinter.Label(text=u'内容')
+Static2.pack()
+
+EditBox2 = tkinter.Entry(width=50)
+EditBox2.pack()
+
+border2=ttk.Separator(root,orient="horizontal")
+border2.pack(fill="both")
+
+Button = tkinter.Button(text=u'文字を送る。',width=40)
+Button.bind("<Button-1>",main) 
+Button.pack()
+
+Button = tkinter.Button(text=u'画像を送る。',width=40)
+Button.bind("<Button-1>",image) 
+Button.pack()
+
+root.mainloop()
--- Send/main.py
+++ Send/main.py
@@ -1,51 +1,59 @@
 #インポート
+from linebot.models import TextSendMessage
+from linebot import LineBotApi
 from tkinter import filedialog
 from tkinter import messagebox
-import requests, json
-import sys
-import tkinter
 import tkinter.ttk as ttk
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
+import requests
+import tkinter
+import json
+import sys
+import os
 
 #Tikinterの基本設定
 root = tkinter.Tk()
 root.title(u"LINE送信パネル")
-root.geometry("310x150")
+root.geometry("310x210")
 root.resizable(0,0)
 
 #ファンクションプログラム
-def main(event):
-    value = EditBox1.get()
-    #作者のUserIDは"U3f694234eb3d4a5feba203c28284f1bf"です。
+def main():
+    accesstoken = LineBotApi(EditBox3.get())
     user_id = EditBox2.get()
-    #作者のトークンは"V8mKR62V0YtYk1ESTzFlmNGdAVkNo+EQEBQgmhyscrOYS5PNd4ai/yXP6E7zcAgVS3vZ+cF+TyfrpwZWduL6hudy+vSrhq8qadl1Nm8OAaxNZoTOA6vUrSHxIy0EeZqr+wNx1FxEBHurgSXTCtB84wdB04t89/1O/w1cDnyilFU="です。
-    YOUR_CHANNEL_ACCESS_TOKEN = EditBox3.get()
-    line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-    #送信設定
-    messages = TextSendMessage(text=value)
-    line_bot_api.push_message(user_id, messages=messages)
-    EditBox1.delete(0, tkinter.END)
-    messagebox.showinfo('LINE送信パネル','送信しました。')
+    messages = TextSendMessage(text=EditBox1.get())
+    accesstoken.push_message(user_id, messages)
 
 def File(event):
-    #ファイル参照
-    FileType = [('Jsonファイル','*.json')] 
-    Dir = 'C:/'
-    File = filedialog.askopenfilename(filetypes = FileType, initialdir = Dir) 
-    json_open = open(File,'r')
+    json_open = open('Deta.json', 'r')
     json_load = json.load(json_open)
-    UserID = json_load['UserID']['key']
     Token = json_load['Token']['key']
-    print("UserID: " + UserID)
-    print("Token: " + Token)
+    UserID =  json_load['UserID']['key']
+    EditBox2.delete(0)
+    EditBox3.delete(0)
+    EditBox2.insert(0,UserID)
+    EditBox3.insert(0,Token)
+    messagebox.showinfo("LINE送信パネル","ユーザーID、アクセストークンを読み込みました。")
+
+def Save(event):
+    Token = EditBox3.get()
+    UserID = EditBox2.get()
+    with open('Deta.json', 'w') as SaveFile:
+        print('', file=SaveFile)
+        print('{"Token":{"key":"' + Token + '"},"UserID":{"key":"' + UserID + '"}}', file=SaveFile)
+    messagebox.showinfo("LINE送信パネル","ユーザーID、アクセストークンを保存しました。")
 
 #UI
+Static1 = tkinter.Label(text=u'メッセージ内容')
+Static1.pack()
+
 EditBox1 = tkinter.Entry(width=50)
 EditBox1.pack()
 
 border1=ttk.Separator(root,orient="horizontal")
 border1.pack(fill="both")
+
+Static2 = tkinter.Label(text=u'宛先（ユーザーID経由）')
+Static2.pack()
 
 EditBox2 = tkinter.Entry(width=50)
 EditBox2.pack()
@@ -53,18 +61,25 @@ EditBox2.pack()
 border2=ttk.Separator(root,orient="horizontal")
 border2.pack(fill="both")
 
+Static1 = tkinter.Label(text=u'チャネルアクセストークン')
+Static1.pack()
+
 EditBox3 = tkinter.Entry(width=50)
 EditBox3.pack()
 
 border3=ttk.Separator(root,orient="horizontal")
 border3.pack(fill="both")
 
-Button = tkinter.Button(text=u'送信（上からメッセージ、ユーザーID、トークンの順に入れる。）',width=50)
+Button = tkinter.Button(text=u'LINEを送る。',width=40)
 Button.bind("<Button-1>",main) 
 Button.pack()
 
-Button2 = tkinter.Button(text=u'保存しているファイルを開く',width=50)
+Button2 = tkinter.Button(text=u'ファイルを開く',width=40)
 Button2.bind("<Button-1>",File) 
+Button2.pack()
+
+Button2 = tkinter.Button(text=u'保存する',width=40)
+Button2.bind("<Button-1>",Save) 
 Button2.pack()
 
 root.mainloop()
